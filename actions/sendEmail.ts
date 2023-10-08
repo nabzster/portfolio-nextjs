712e1df6359bbb12486456b9a07797c3bed2c84e
Nabz <alfred.alfonso@hotmail.com>
2023-10-08 12:07:42 -0400

    Added some email functionality

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,34 @@
 "use server"
 
+import { Resend } from 'resend';
+import { validateString } from '@/lib/utils';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-    console.log("Running on server")
-    console.log(formData.get("senderEmail"));
-    console.log(formData.get("message"));
+    const message = formData.get('message');
+    const senderEmail = formData.get('senderEmail');
+
+    if (!validateString(message, 5000)) {
+        return {
+            error: "Invalid message"
+        }
+    }
+    if (!validateString(senderEmail, 500)) {
+        return {
+            error: "Invalid sender email"
+        }
+    }
+
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: 'alfred.alfonso@hotmail.com',
+            subject: 'Message from contact form',
+            reply_to: senderEmail as string,
+            text: message as string
+        })
+    } catch (error) {
+        console.log(error);
+    }
 };
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -25,10 +25,6 @@ export default function Contact() {
 
             <form className="mt-10 flex flex-col"
             action={async (formData) => {
-                console.log("Running on client")
-                console.log(formData.get("senderEmail"));
-                console.log(formData.get("message"));
-
                 await sendEmail(formData);
             }}>
                 <input className="h-14 rounded-lg borderBlack px-4" 
@@ -39,7 +35,7 @@ export default function Contact() {
                 <textarea className="h-52 my-3 rounded-lg borderBlack p-4"
                 name="message" 
                 required 
-                maxLength={500}
+                maxLength={5000}
                 placeholder="Your Message"/>
                 <button type="submit" className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105">
                     Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>
